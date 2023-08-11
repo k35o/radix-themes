7a4f011602cb64cc58a048f98cddd32a943a2964
Koki Sakano <kosakanoki@gmail.com>
2023-08-11 15:37:18 +0900

    エラーはいじょ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Button, Flex, Text, ThemePanel } from '@radix-ui/themes'
 export default function Home() {
   return (
     <Flex direction="column" gap="2">
-      <Text>Hello from Radix Themes :)</Text>
-      <Button>Let's go</Button>
+      <Text>Hello from Radix Themes &rsquo;)</Text>
+      <Button>Let&rsquo;s go</Button>
       <ThemePanel />
     </Flex>
   )
